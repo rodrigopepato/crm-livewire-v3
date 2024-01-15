db41f22a32a25da29c2af3aafe8cfc8a7731eea1
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-01-15 20:11:51 -0300

    crm-1: validation rulesto our registration form

--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -28,3 +28,21 @@ it('should be able to register a new user in the system', function () {
     assertDatabaseCount('users', 1);
 
 });
+
+test('validation rules', function ($f) {
+
+    Livewire::test(Register::class)
+        ->set($f->field, $f->value)
+        ->call('submit')
+        ->assertHasErrors([$f->field => $f->rule]);
+
+})->with([
+    'name::required'     => (object)['field' => 'name', 'value' => '', 'rule' => 'required'],
+    'name::max:255'      => (object)['field' => 'name', 'value' => str_repeat('*', 256), 'rule' => 'max'],
+    'email::required'    => (object)['field' => 'email', 'value' => '', 'rule' => 'required'],
+    'email::email'       => (object)['field' => 'email', 'value' => 'not-an-email', 'rule' => 'email'],
+    'email::max:255'     => (object)['field' => 'email', 'value' => str_repeat('*' . '@doe.com', 256), 'rule' => 'max'],
+    'email::confirmed'   => (object)['field' => 'email', 'value' => 'joe@doe.com', 'rule' => 'confirmed'],
+    'password::required' => (object)['field' => 'password', 'value' => '', 'rule' => 'required'],
+
+]);
