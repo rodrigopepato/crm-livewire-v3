5418ab66ae4734cbaf308b192282ac3920be4389
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-01-24 17:26:22 -0300

    crm-2: ensuring login to be secured by rate limiting

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -3,7 +3,8 @@
 namespace App\Livewire\Auth;
 
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\{Auth, RateLimiter};
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Login extends Component
@@ -19,8 +20,13 @@ class Login extends Component
 
     public function tryToLogin(): void
     {
+        if($this->ensureIsNotRateLimiting()) {
+            return;
+        }
 
         if(!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+
+            RateLimiter::hit($this->throttleKey());
 
             $this->addError('invalidCredentials', trans('auth.failed'));
 
@@ -28,5 +34,25 @@ class Login extends Component
         }
 
         $this->redirect(route('dashboard'));
+    }
+
+    private function throttleKey(): string
+    {
+
+        return Str::transliterate(Str::lower($this->email) . '|' . request()->ip());
+    }
+
+    private function ensureIsNotRateLimiting()
+    {
+        if(RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+
+            $this->addError('rateLimiter', trans('auth.throttle', [
+                'seconds' => RateLimiter::availableIn($this->throttleKey()),
+            ]));
+
+            return true;
+        }
+
+        return false;
     }
 }
--- tests/Feature/Livewire/Auth/LoginTest.php
+++ tests/Feature/Livewire/Auth/LoginTest.php
@@ -38,3 +38,23 @@ it('should make sure to inform the user an error when email and password doesnt 
         ->assertSee(trans('auth.failed'));
 
 });
+
+it('should make sure that the rate limiting is blocking after 5 attempts', function () {
+
+    $user = User::factory()->create();
+
+    for($i = 0; $i < 5; $i++) {
+        Livewire::test(Login::class)
+            ->set('email', $user->email)
+            ->set('password', 'wrong-password')
+            ->call('tryToLogin')
+            ->assertHasErrors(['invalidCredentials']);
+    }
+
+    Livewire::test(Login::class)
+            ->set('email', $user->email)
+            ->set('password', 'wrong-password')
+            ->call('tryToLogin')
+            ->assertHasErrors(['rateLimiter']);
+
+});
