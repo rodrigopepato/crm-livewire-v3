f87628313819af760bc7413d594ca222d95f625a
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-01-31 23:54:58 -0300

    crm-4: it should be able to request for a password recovery seding notification to the user

---
+++ resources/views/livewire/auth/password/recovery.blade.php
@@ -0,0 +1,10 @@
+<div>
+    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+
+    @if ($message)
+
+        <span>You will receive an email with the password recovery link.</span>
+
+    @endif
+
+</div>
--- tests/Feature/Auth/PasswordRecoveryTest.php
+++ tests/Feature/Auth/PasswordRecoveryTest.php
@@ -1,10 +1,36 @@
 <?php
 
+use App\Livewire\Auth\Password\Recovery;
+use App\Models\User;
+use App\Notifications\PasswordRecoveryNotification;
+use Illuminate\Support\Facades\Notification;
+use Livewire\Livewire;
+
 use function Pest\Laravel\get;
 
 test('needs to have a route to password recovery', function () {
 
     get(route('auth.password.recovery'))
         ->assertOk();
+
+});
+
+it('should be able to request for a password recovery seding notification to the user', function () {
+
+    Notification::fake();
+
+    /** @var User $user */
+    $user = User::factory()->create();
+
+    Livewire::test(Recovery::class)
+        ->assertDontSee('You will receive an email with the password recovery link.')
+        ->set('email', $user->email)
+        ->call('startPasswordRecovery')
+        ->assertSee('You will receive an email with the password recovery link.');
+
+    Notification::assertSentTo(
+        $user,
+        PasswordRecoveryNotification::class
+    );
 
 });
