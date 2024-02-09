598c5bffea404cc4e1698d456d69ed6f22cfba80
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-02-09 14:13:07 -0300

    crm-5: user model has all methods to control permission for the given user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -29,4 +30,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password'          => 'hashed',
     ];
+
+    public function permissions(): BelongsToMany
+    {
+
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function givePermissionTo(string $key): void
+    {
+
+        $this->permissions()->firstOrCreate(compact('key'));
+    }
+
+    public function hasPermissionTo(string $key): bool
+    {
+
+        return $this->permissions()->where(compact('key'))->exists();
+    }
 }
