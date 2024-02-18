058e7677e9c5517c36f9cd12fc3c9a111060a2e4
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-02-17 23:29:15 -0300

    crm-12: installing @tailwindcss/forms @tailwindcss/typography

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -1,21 +1,7 @@
 <div>
     <x-header title="Users" separator/>
 
-
-    <div class="mb-4 flex space-x-4">
-        <div class="w-1/3">
-            <x-input
-                icon="o-magnifying-glass"
-                class="input-sm "
-                placeholder="Search by email and name"
-                wire:model.live="search"
-            />
-        </div>
-        <x-select class="select-sm"> // Permission
-            <option value="">asldkjasd</option>
-        </x-select>
-    </div>
-
+    
 
     <x-table :headers="$this->headers" :rows="$this->users">
         @scope('cell_permissions', $user)
