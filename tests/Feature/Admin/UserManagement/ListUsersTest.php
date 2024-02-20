f148475d26b65a38cafae79614f76b5c97e87fbb
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-02-20 16:27:07 -0300

    crm-12: fixing test because we changed the format of the header output

--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -49,10 +49,10 @@ test('check the table format', function () {
 
     Livewire::test(Admin\Users\Index::class)
         ->assertSet('headers', [
-            ['key' => 'id', 'label' => '#'],
-            ['key' => 'name', 'label' => 'Name'],
-            ['key' => 'email', 'label' => 'Email'],
-            ['key' => 'permissions', 'label' => 'Permissions'],
+            ['key' => 'id', 'label' => '#', 'sortColumnBy' => 'id', 'sortDirection' => 'asc'],
+            ['key' => 'name', 'label' => 'Name', 'sortColumnBy' => 'id', 'sortDirection' => 'asc'],
+            ['key' => 'email', 'label' => 'Email', 'sortColumnBy' => 'id', 'sortDirection' => 'asc'],
+            ['key' => 'permissions', 'label' => 'Permissions', 'sortColumnBy' => 'id', 'sortDirection' => 'asc'],
         ]);
 });
 
