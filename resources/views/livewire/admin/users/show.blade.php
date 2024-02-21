33006319313c8f69ab29e3e5aeccad4f33e455fe
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-02-21 18:30:09 -0300

    crm-13: displaying the user information inside a layout

--- resources/views/livewire/admin/users/show.blade.php
+++ resources/views/livewire/admin/users/show.blade.php
@@ -1,12 +1,17 @@
 <div>
-    @if($user)
-
-        {{ $user->name }}
-        {{ $user->email }}
-        {{ $user->created_at->format('d/m/Y H:i') }}
-        {{ $user->updated_at->format('d/m/Y H:i') }}
-        {{ $user->deleted_at?->format('d/m/Y H:i') }}
-        {{ $user->deletedBy?->name }}
-
-    @endif
+    <x-modal wire:model="modal" :title="$user?->name" separator>
+        @if($user)
+            <div class="space-y-2">
+                <x-input readonly label="Name" :value="$user->name"/>
+                <x-input readonly label="Email" :value="$user->email"/>
+                <x-input readonly label="Created At" :value="$user->created_at->format('d/m/Y H:i')"/>
+                <x-input readonly label="Updated At" :value="$user->updated_at->format('d/m/Y H:i')"/>
+                <x-input readonly label="Deleted At" :value="$user->deleted_at?->format('d/m/Y H:i')"/>
+                <x-input readonly label="Deleted By" :value="$user->deletedBy?->name"/>
+            </div>
+        @endif
+        <x-slot:actions>
+            <x-button label="Cancel" @click="$wire.modal = false"/>
+        </x-slot:actions>
+    </x-modal>
 </div>
