f70dbaaae4c025ac34931fe829844f2a27a8e1f2
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-02-21 23:08:11 -0300

    crm-7: adding the impersonator when auditing

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,14 +8,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use OwenIt\Auditing\Auditable as AuditableTrait;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Auditable
 {
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
     use HasPermissions;
     use SoftDeletes;
+    use AuditableTrait;
 
     protected $fillable = [
         'name',
