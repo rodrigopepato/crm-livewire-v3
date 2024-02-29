0bc04dc80f5df390ca86421b6bcce0c06571f02b
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-02-29 13:06:37 -0300

    crm-11: validation page: redirect after registration

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Listeners\Auth\CreateValidationCode;
+use App\Livewire\Auth\Register;
 use App\Models\User;
 use App\Notifications\Auth\ValidationCodeNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\{Event, Notification};
+use Livewire\Livewire;
 
 use function PHPUnit\Framework\assertTrue;
 
@@ -12,38 +14,57 @@ beforeEach(function () {
     Notification::fake();
 });
 
-it("should create a new validation code and save in the users table", function () {
-    $user = User::factory()->create(['email_verified_at' => null, 'validation_code' => null]);
+describe('after registration', function () {
 
-    $event    = new Registered($user);
-    $listener = new CreateValidationCode();
-    $listener->handle($event);
+    it("should create a new validation code and save in the users table", function () {
+        $user = User::factory()->create(['email_verified_at' => null, 'validation_code' => null]);
 
-    $user->refresh();
+        $event    = new Registered($user);
+        $listener = new CreateValidationCode();
+        $listener->handle($event);
 
-    expect($user)->validation_code->not->toBeNull()
-        ->and($user)->validation_code->toBeNumeric();
+        $user->refresh();
 
-    assertTrue(str($user->validation_code)->length() == 6);
+        expect($user)->validation_code->not->toBeNull()
+            ->and($user)->validation_code->toBeNumeric();
+
+        assertTrue(str($user->validation_code)->length() == 6);
+
+    });
+
+    it('should send that new code to the user via email', function () {
+
+        $user = User::factory()->create(['email_verified_at' => null, 'validation_code' => null]);
+
+        $event    = new Registered($user);
+        $listener = new CreateValidationCode();
+        $listener->handle($event);
+
+        Notification::assertSentTo($user, ValidationCodeNotification::class);
+    });
+
+    test('making sure that the listener to send the code is linked to the Registered event', function () {
+
+        Event::fake();
+        Event::assertListening(
+            Registered::class,
+            CreateValidationCode::class
+        );
+    });
 
 });
 
-it('should send that new code to the user via email', function () {
+describe('validation page', function () {
 
-    $user = User::factory()->create(['email_verified_at' => null, 'validation_code' => null]);
+    it('should redirect to the validation page after registration', function () {
+        Livewire::test(Register::class)
+            ->set('name', 'Joe doe')
+            ->set('email', 'joe@doe.com')
+            ->set('email_confirmation', 'joe@doe.com')
+            ->set('password', 'password')
+            ->call('submit')
+            ->assertHasNoErrors()
+            ->assertRedirect(route('auth.email-validation'));
+    });
 
-    $event    = new Registered($user);
-    $listener = new CreateValidationCode();
-    $listener->handle($event);
-
-    Notification::assertSentTo($user, ValidationCodeNotification::class);
-});
-
-test('making sure that the listener to send the code is linked to the Registered event', function () {
-
-    Event::fake();
-    Event::assertListening(
-        Registered::class,
-        CreateValidationCode::class
-    );
 });
--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -3,7 +3,6 @@
 use App\Livewire\Auth\Register;
 use App\Models\User;
 use App\Notifications\WelcomeNotification;
-use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\{Event, Notification};
 use Livewire\Livewire;
@@ -24,7 +23,7 @@ it('should be able to register a new user in the system', function () {
         ->set('password', 'password')
         ->call('submit')
         ->assertHasNoErrors()
-        ->assertRedirect(RouteServiceProvider::HOME);
+        ->assertHasNoErrors();
 
     assertDatabaseHas('users', [
         'name'  => 'joe Doe',
