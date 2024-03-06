7e6ab55b89fa814b04ddb8e7d18978120b474fba
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-03-06 20:09:14 -0300

    crm-22: crm-22: customer create-update: refactoring to use livewire form

--- app/Livewire/Customers/Update.php
+++ app/Livewire/Customers/Update.php
@@ -2,34 +2,37 @@
 
 namespace App\Livewire\Customers;
 
+use App\Livewire\Forms\Form;
 use App\Models\Customer;
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Update extends Component
 {
-    public Customer $customer;
+    public Form $form;
 
     public bool $modal = false;
 
-    public function rules(): array
-    {
-        return [
-            'customer.name'  => ['required', 'min:3', 'max:255'],
-            'customer.email' => ['required_without:phone', 'email', 'unique:customers,email'],
-            'customer.phone' => ['required_without:email', 'unique:customers,phone'],
-        ];
-    }
     public function render(): View
     {
         return view('livewire.customers.update');
     }
 
+    #[On('customer::update')]
+    public function load(int $id): void
+    {
+        $customer = Customer::find($id);
+        $this->form->setCustomer($customer);
+
+        $this->form->resetErrorBag();
+        $this->modal = true;
+    }
+
     public function save(): void
     {
-        $this->validate();
+        $this->form->update();
 
-        $this->customer->update();
-
+        $this->modal = false;
     }
 }
