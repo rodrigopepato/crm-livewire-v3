54a295736e8ce58b6c8d6b8bac4d05a6a8dc4109
Rodrigo Pepato <132705370+rodrigopepato@users.noreply.github.com>
2024-03-07 16:42:54 -0300

    crm-22:  fixing update submit form

--- resources/views/livewire/customers/update.blade.php
+++ resources/views/livewire/customers/update.blade.php
@@ -1,5 +1,4 @@
 <x-drawer wire:model="modal" title="Updating Customer" class="w-1/3 p-4" right>
-
     <x-form wire:submit="save" id="update-customer-form">
         <hr class="my-5"/>
         <div class="space-y-2">
@@ -12,6 +11,4 @@
             <x-button label="Save" type="submit" form="update-customer-form"/>
         </x-slot:actions>
     </x-form>
-
-
 </x-drawer>
